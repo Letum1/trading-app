cd727b7e10e8c98c5553929a5f1b113007746179
Letum1 <132340173+Letum1@users.noreply.github.com>
2025-11-09 18:44:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-appapp.listen(PORT, () => {app.use(express.static('public'));
-  console.log(`Server running on port ${PORT}`);
-});
+// Serve static files from the public folder
+app.use(express.static('public'));
 
+// Default route
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
